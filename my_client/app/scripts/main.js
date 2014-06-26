214e5a419fc61cfa0acc3a659104430c313d69cc
Alex Moore-Niemi <alex@promoboxx.com>
2014-06-25 22:35:09 -0400

    simple rails api and backbone client

---
+++ my_client/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global MyClient, $*/
+
+
+window.MyClient = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    MyClient.init();
+});
