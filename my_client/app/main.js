ad3f2084140dabf0beb7552dc765ebb4fa0b2939
Alex Moore-Niemi <alex@promoboxx.com>
2014-07-04 14:22:30 -0400

    handling hash urls because they annoy me

--- my_client/app/main.js
+++ my_client/app/main.js
@@ -2,12 +2,24 @@
 'use strict';
 
 require(['require_config'], function() {
-  require([
-    'backbone', 'router'
-  ], function(Backbone, HighScoreRouter) {
-    var appRouter = new HighScoreRouter();
-    Backbone.history.start({
-      pushState: true
+    require([
+        'backbone', 'router'
+    ], function(Backbone, HighScoreRouter) {
+        $(document).on("click", "a:not([data-bypass])", function(evt) {
+            var href = {
+                prop: $(this).prop("href"),
+                attr: $(this).attr("href")
+            };
+            var root = location.protocol + "//" + location.host + Backbone.history.options.root;
+
+            if (href.prop && href.prop.slice(0, root.length) === root) {
+                evt.preventDefault();
+                Backbone.history.navigate(href.attr, true);
+            }
+        });
+        var appRouter = new HighScoreRouter();
+        Backbone.history.start({
+            pushState: true
+        });
     });
-  });
 });
