7983d32bec96546608a0de895252f21b629124ba
Alex Moore-Niemi <alex@promoboxx.com>
2014-07-04 00:25:07 -0400

    fixing test_regexp to exclude vendor specs, and some linting

--- my_client/spec/high_score.spec.js
+++ my_client/spec/high_score.spec.js
@@ -1,24 +1,24 @@
 /*global describe, it */
 'use strict';
 
-// define(['view'], function(HighScoreView) {
-//     describe('Integration test', function() {
-//         describe('displaying scores', function() {
-//             it('should fetch and render correctly from my_api', function() {
-//                 expect(new HighScoreView).toBeDefined();
-//             });
-//         });
-//     });
-// });
+define(['view'], function(HighScoreView) {
+    describe('Integration test', function() {
+        describe('displaying scores', function() {
+            it('should fetch and render correctly from my_api', function() {
+                expect(new HighScoreView).toBeDefined();
+            });
+        });
+    });
+});
 
 define(function(require) {
-  'use strict';
+    'use strict';
 
-  require('jquery');
-  require('underscore');
-  require('handlebars');
+    require('jquery');
+    require('underscore');
+    require('handlebars');
 
-  describe('just checking', function() {
+    describe('just checking', function() {
         it('works for underscore', function() {
             // just checking that _ works
             expect(_.size([1, 2, 3])).toEqual(3);
--- my_client/test-main.js
+++ my_client/test-main.js
@@ -1,45 +1,45 @@
 var allTestFiles = [];
-var TEST_REGEXP = /spec\.js$/i;
+var TEST_REGEXP = /\.spec\.js$/i;
 
 var pathToModule = function(path) {
-  return path.replace(/^\/base\//, '../').replace(/\.js$/, '');
+    return path.replace(/^\/base\//, '../').replace(/\.js$/, '');
 };
 
 Object.keys(window.__karma__.files).forEach(function(file) {
-  if (TEST_REGEXP.test(file)) {
-    // Normalize paths to RequireJS module names.
-    allTestFiles.push(pathToModule(file));
-  }
+    if (TEST_REGEXP.test(file)) {
+        // Normalize paths to RequireJS module names.
+        allTestFiles.push(pathToModule(file));
+    }
 });
 
 require.config({
-  // Karma serves files under /base, which is the basePath from your config file
-  baseUrl: '/base/app',
+    // Karma serves files under /base, which is the basePath from your config file
+    baseUrl: '/base/app',
 
-  paths: {
-    'view': 'views/high_score',
-    'jasmine-jquery': 'bower_components/jasmine-jquery/lib/jasmine-jquery',
-    'jasmine-ajax': 'bower_components/jasmine-ajax/lib/mock-ajax',
-    'jasmine-fixture': 'bower_components/jasmine-fixture/dist/jasmine-fixture'
-  },
-
-  shim: {
-    'jasmine-jquery': {
-      deps: ['jquery'],
-      exports: 'jasmine'
+    paths: {
+        'view': 'views/high_score',
+        'jasmine-jquery': 'bower_components/jasmine-jquery/lib/jasmine-jquery',
+        'jasmine-ajax': 'bower_components/jasmine-ajax/lib/mock-ajax',
+        'jasmine-fixture': 'bower_components/jasmine-fixture/dist/jasmine-fixture'
     },
-    'jasmine-ajax': {
-      deps: ['jasmine-jquery', 'jquery'],
-      exports: 'jasmine'
+
+    shim: {
+        'jasmine-jquery': {
+            deps: ['jquery'],
+            exports: 'jasmine'
+        },
+        'jasmine-ajax': {
+            deps: ['jasmine-jquery', 'jquery'],
+            exports: 'jasmine'
+        },
+        'jasmine-fixture': {
+            deps: ['jquery']
+        }
     },
-    'jasmine-fixture': {
-      deps: ['jquery']
-    }
-  },
 
-  // dynamically load all test files
-  deps: allTestFiles,
+    // dynamically load all test files
+    deps: allTestFiles,
 
-  // we have to kickoff jasmine, as it is asynchronous
-  callback: window.__karma__.start
+    // we have to kickoff jasmine, as it is asynchronous
+    callback: window.__karma__.start
 });
